11c5cbed4f0b0e816a352f86d074bab44859655b
Cirlig Petru <cirligpetru96@gmail.com>
2026-05-22 23:41:45 +0200

    Initial commit

---
+++ src/main/java/PA/PrimulExercitiu/PrimulExercitiuApplication.java
@@ -0,0 +1,13 @@
+package PA.PrimulExercitiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimulExercitiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimulExercitiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PA/PrimulExercitiu/PrimulExercitiuApplicationTests.java
@@ -0,0 +1,13 @@
+package PA.PrimulExercitiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimulExercitiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
