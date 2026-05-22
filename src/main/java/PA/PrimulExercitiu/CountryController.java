6963d8cb2d911d64dd0d749545398f9ebe082c50
Cirlig Petru <cirligpetru96@gmail.com>
2026-05-22 23:51:50 +0200

    Add Country and Capitals

--- src/main/java/PA/PrimulExercitiu/CountryController.java
+++ src/main/java/PA/PrimulExercitiu/CountryController.java
@@ -1,7 +1,25 @@
 package PA.PrimulExercitiu;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class CountryController {
+
+    @GetMapping("/Moldova")
+        public String Moldova() {
+        return "Capitala este Chisinau";
+    }
+
+    @GetMapping("/France")
+    public String France() {
+        return "Capitala este Paris";
+    }
+
+    @GetMapping("/Germany")
+    public String Germany() {
+        return "Capitala este Berlin";
+    }
+
 }
